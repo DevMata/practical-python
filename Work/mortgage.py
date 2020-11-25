5b3f02ccc7f8093e5bcdc11c436aca8d20b39d1b
Antonio Mata <hildeamp@gmail.com>
2020-11-25 11:36:13 -0600

    feat: add mortgage extra payment logic

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -5,9 +5,17 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+months = 0
+extra_payment = 1000
 
 while principal > 0:
-    principal = principal * (1+rate/12) - payment
+    principal = principal * (1 + rate / 12) - payment
+    months = months + 1
+
+    if months <= 12:
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
+
     total_paid = total_paid + payment
 
-print('Total paid', total_paid)
+print('Total paid', total_paid, 'Months', months)
